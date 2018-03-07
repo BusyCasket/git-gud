b6effd759e04b468157ac4cd2aaa93f09458f218
BusyCasket <cassian.knoth@hotmail.de>
2018-03-07 21:21:16 +0100

    Hallo neu!

--- javascript.js
+++ javascript.js
@@ -32,11 +32,15 @@ function setSeparatorDiv(){
 	document.getElementById("separator").innerHTML = "/";
 }
 
+function setSeparatorPot(){
+	document.getElementById("separator").innerHTML = "^";
+}
+
 function solve2(){
 	var numberOne = Number(document.getElementById("firstNumber").value);
 	var numberTwo = Number(document.getElementById("secondNumber").value);
 	var sep = document.getElementById("separator").innerHTML;
-	var text = ""
+	var text = "";
 	
 	switch(sep) {
 		
@@ -56,10 +60,12 @@ function solve2(){
 		text += numberOne / numberTwo;
 		break;
 		
+		case "^":
+		text = Math.pow(numberOne, numberTwo);
+		break;
+		
 		default:
 		text += numberOne + numberTwo;
-		
-		
 	}
 	
 	document.getElementById("result2").innerHTML = text;
@@ -81,7 +87,7 @@ function loddo(){
 	var z = 0;
 	
 	
-	for (i = 0; i < 6; i++) {
+	for (var i = 0; i < 6; i++) {
 		var y = document.getElementById("guess"+i).value;
 		
 		if (y === ""){
@@ -94,7 +100,7 @@ function loddo(){
 			return;
 		}
 		
-		if (y < 1, y > 49) {
+		if (y < 1  || y > 49) {
 			alert("Es sind nur Zahlen von 1 - 49 erlaubt!");
 			return;
 		}
@@ -109,7 +115,7 @@ function loddo(){
 	}
 	
 	
-	for (i = 0; i < 6; i++){
+	for (var i = 0; i < 6; i++){
 		var x = Math.floor((Math.random() * (49 - 1)) + 1);
 		
 		if (checkDoubles(drawn, x)){
@@ -121,41 +127,43 @@ function loddo(){
 	}
 	
 	
-	for (i = 0; i < 6; i++){
+	for (var i = 0; i < 6; i++){
 		var guess = guesses[i];
-		if (compare(drawn, guess)){
+		if (checkDoubles(drawn, guess)){
 			z = z + 1;
 		}
 	}
 	
 	switch(z){
 		
-		case 0:
-		return;
-		break;
-		
 		case 1:
-		money = money + 5;
+		money = money + 1;
+		playAudio();
 		break;
 		
 		case 2:
-		money = money + 25;
+		money = money + 5;
+		playAudio();
 		break;
 		
 		case 3:
 		money = money + 500;
+		playAudio();
 		break;
 		
 		case 4:
 		money = money + 50000;
+		playAudio();
 		break;
 		
 		case 5:
 		money = money + 5000000;
+		playAudio();
 		break;
 		
 		case 6:
 		money = money + 500000000;
+		playAudio();
 	}
 	
 	
@@ -170,7 +178,7 @@ function loddo(){
 }
 
 function checkDoubles(array, element) {
-	for (i = 0;i < array.length; i++){
+	for (var i = 0;i < array.length; i++){
 		if (array[i] == element){
 			return true;
 		} 	
@@ -179,11 +187,102 @@ function checkDoubles(array, element) {
 	return false;
 }
 
-function compare(array, element){
-	for (a = 0; a < array.length; a++){
-		if (array[i] == element){
-			return true;
-		}
+
+
+function playAudio(){
+	
+	var cashSound = document.getElementById("cashin");
+	
+	cashSound.play();
+}
+
+
+// Circle Run
+
+function wuerfeln() {
+	var zahl = Math.floor((Math.random()*6) + 1);
+	var text = "";
+	
+	text += zahl;
+	
+	document.getElementById("wuerfelErgebnis").innerHTML = text;
+}
+
+var currentField = 0;
+
+function ziehen() {
+	
+	
+	var ziehen = document.getElementById("wuerfelErgebnis").innerHTML;
+	
+	var field = currentField;
+	
+	switch(ziehen) {
+		case "?":
+		alert("Du musst erst würfeln!");
+		break;
+		
+		case "1":
+		field += 1;
+		break;
+		
+		case "2":
+		field += 2;
+		break;
+		
+		case "3":
+		field += 3;
+		break;
+		
+		case "4":
+		field += 4;
+		break;
+		
+		case "5":
+		field += 5;
+		break;
+		
+		case "6":
+		field += 6;
+		break;
+		
+		default:
+		alert("Irgendwas ist schiefgelaufen...");
 	}
-	return false;
+	
+	
+	
+	if ((currentField + field) <= 7){
+		
+		var colorField = currentField + field;
+		
+		for (i = 0; i < 7; i++){
+			if (i == colorField) {
+				document.getElementById("spielfeld" + i).style.background.color = "red";
+				currentField = colorField;
+			} else {
+				document.getElementById("spielfeld" + i).style.background.color = "white";
+				currentField = colorField;
+			}
+		}
+		
+		
+	} else {
+		
+		colorField -= 8;
+		
+		for (i = 0; i < 7; i++){
+			if (i == colorField) {
+				document.getElementById("spielfeld" + i).style.background.color = "red";
+				currentField = colorField;
+			} else {
+				document.getElementById("spielfeld" + i).style.background.color = "white";
+				currentField = colorField;
+			}
+		}
+		
+	}
+	
+	document.getElementById("wuerfelErgebnis").innerHTML = "?";
+	
 }
